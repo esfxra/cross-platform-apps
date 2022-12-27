f5c551c22167c2065fd0ec8b12c0dfe81b2da832
Diego <diegoserranor@gmail.com>
2022-12-27 18:52:26 -0500

    feat: ejected native app

---
+++ apps/bare/App.tsx
@@ -0,0 +1,19 @@
+import { SafeAreaView } from "react-native";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { StatusBar } from "expo-status-bar";
+import CoinList from "./components/CoinList";
+
+const queryClient = new QueryClient();
+
+const BareApp = () => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <SafeAreaView style={{ flex: 1, backgroundColor: "#fff" }}>
+        <CoinList />
+      </SafeAreaView>
+      <StatusBar style="auto" />
+    </QueryClientProvider>
+  );
+};
+
+export default BareApp;
